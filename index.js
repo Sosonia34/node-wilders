3342a2ccf0d6164a7066c8a0d8499a9025885dd5
Sonia34sonia <sonia.vidal@sncf.fr>
2022-05-09 14:41:21 +0200

    exercice nodejs

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+const myName = ["loup", "daniel", "nicolas", "florence"];
+console.log(myName)
+
+console.log(chalk.blue(`salut ${myName[0]}`));
+console.log(chalk.red(`coucou ${myName[1]}`));
+console.log(chalk.yellow(`bienvenu ${myName[2]}`));
+console.log(chalk.green(`bonjour ${myName[3]}`));
